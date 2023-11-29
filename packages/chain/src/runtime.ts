fdee0fcaf818523698d8ea978df7ae14e8163c16
Raunaque97 <rpatra1997@gmail.com>
2023-11-30 05:13:05 +0530

    switched to using npm scoped package for runtimes

--- packages/chain/src/runtime.ts
+++ packages/chain/src/runtime.ts
@@ -1,13 +1,21 @@
-import { Balances } from "./Balances";
-import { NFT } from "./NFT";
+import {
+  NFT,
+  Balances,
+  EnglishAuctionModule,
+  DutchAuctionModule,
+} from "@reevl/protokit-runtimes";
 
 export default {
   modules: {
     Balances,
     NFT,
+    EnglishAuctionModule,
+    DutchAuctionModule,
   },
   config: {
     Balances: {},
     NFT: {},
+    EnglishAuctionModule: {},
+    DutchAuctionModule: {},
   },
 };
